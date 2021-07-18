34f6d470a22931ab03085081ff923536032d933c
victoria lein <>
2021-07-18 19:20:34 -0400

    school management system por sistema de gestión escolar

--- side-menu.php
+++ side-menu.php
@@ -75,7 +75,7 @@
 					<span class="icon-bar bar2"></span>
 					<span class="icon-bar bar3"></span>
 				</button>
-				<a class="navbar-brand" href="#">School Management System</a>
+				<a class="navbar-brand" href="#">Sistema de gestión escolar</a>
 			</div>
 			<div class="collapse navbar-collapse">
 				<ul class="nav navbar-nav navbar-right">
